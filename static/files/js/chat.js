ccd690b7360b877f7248d15db7adf80a1e503219
jerapepe <ruizpepe402@gmail.com>
2024-04-04 18:17:23 -0600

    chat

--- static/files/js/chat.js
+++ static/files/js/chat.js
@@ -1,32 +1,30 @@
-let formulario = document.getElementById("chatbox");
-let messages = document.getElementById("messages");
-let username = document.getElementById("username");
-let text = document.getElementById("message");
-let buttonConect = document.getElementById("conect");
-let buttonSend = document.getElementById("send");
-let socket = new WebSocket("ws://localhost:8000/room");
+const nameUser = prompt("Por favor, introduce tu nombre:");
+let messageInput = document.getElementById("messageInput");
+let messageList = document.getElementById("messageList");
+let buttonSend = document.getElementById("sendMessage");
+let socket = new WebSocket("ws://192.168.0.215:8000/room");
 
-formulario?.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (!socket) {
-    alert("Error: There is no socket connection.");
-    return false;
+if (!nameUser) {
+  alert("Por favor, introduce tu nombre");
+  location.reload();
+}
+
+buttonSend?.addEventListener("click", () => {
+  let message = messageInput.value;
+  if (message) {
+    let msg = {
+      username: nameUser,
+      text: message
+    };
+    socket.send(JSON.stringify(msg));
+    messageInput.value = "";
   }
-  let msg = {
-    username: username.value,
-    text: text.value
-  };
-  socket.send(JSON.stringify(msg));
-  console.log(msg);
-  console.log("Mensaje enviado");
-  text.value = "";
-  return false;
 });
 
 socket.onmessage = function(event) {
   var msg = JSON.parse(event.data);
   console.log("Received message:", msg);
-  messages.innerHTML += `<li>${msg.username}: ${msg.text}</li>`;
+  messageList.innerHTML += `<li>${msg.username}: ${msg.text}</li>`;
 };
 
 socket.onclose = function() {
